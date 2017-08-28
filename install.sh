d8028d479be82ccf48264fad1ba9a222e840ce2f
Eric Bailey <ericbaileydrums@gmail.com>
2017-08-28 04:32:14 -0400

    update install.sh

--- install.sh
+++ install.sh
@@ -2,4 +2,18 @@
 
 [ -d ~/bin ] || mkdir ~/bin
 
-for i in *; do [ $i = $(basename $0) ] && continue || ln -s $(pwd)/$i ~/bin/$i; done
+for i in *; do
+    if [ $i = $(basename $0) -o -d $i ]; then
+        continue
+    else
+        ln -s $(pwd)/$i ~/bin/$i && echo Created ~/bin/$i || failure=1
+    fi
+done
+
+read -p "Install xpswifi? (y/N): " choice
+case $choice in
+    y|Y )
+        ln -s xpswifi/connect-wifi.sh ~/bin/xpswifi;;
+
+    * )
+esac
