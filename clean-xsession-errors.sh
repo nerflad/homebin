6279125f00205d78a5117045ae231b9d99f09272
Eric Bailey <ericbaileydrums@gmail.com>
2019-09-29 16:03:29 -0400

    clean-xsession-errors.sh: fix typos

--- clean-xsession-errors.sh
+++ clean-xsession-errors.sh
@@ -1,3 +1,3 @@
 #!/bin/bash
 
-[ $(du -k .xsession-errors | awk '{ print $1 }') -gt 5000000 ] && tail -10000 /home/$(whoami)/.xsession-errors > /home/$(whoami)/.xsession-errors
+[ $(du -k ~/.xsession-errors | awk '{ print $1 }') -gt 5000000 ] && tail -10000 ~/.xsession-errors > ~/.xsession-errors
